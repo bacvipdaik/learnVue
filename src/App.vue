4c1038e466059effb85968e3fe6c79e37c44411b
bacvipdaik <100465809+bacvipdaik@users.noreply.github.com>
2023-03-20 12:32:06 +0700

    'abc'

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import Modal from "./components/Modal.vue";
+export default {
+  data() {
+    return {
+      isShowModal: false,
+    };
+  },
+  methods: {
+    onTongleModal() {
+      this.isShowModal = !this.isShowModal;
+    },
+  },
+};
+</script>
+
+<template>
+  <Teleport to="body">
+    <Modal
+      v-if="isShowModal"
+      title="This is new title from app componemt"
+      content="This is content modal"
+      theme="contact"
+      @cancle="onTongleModal"
+    >
+      <template v-slot:header>
+        <h2>This is header</h2>
+      </template>
+      <template v-slot:footer>
+        <button @click="onTongleModal">Cancle</button>
+      </template>
+      <label for="">Name </label>
+      <input type="password" />
+    </Modal>
+  </Teleport>
+  <button @click="onTongleModal">Tonggle modal</button>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import './assets/Styles/global.css'
+
+createApp(App).mount('#app')
